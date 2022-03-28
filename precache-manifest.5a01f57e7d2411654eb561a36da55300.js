f26b17bf2f011114f89ef556c224e97d69b65aad
Ankit Rauniyar <rauniyar2012@gmail.com>
2022-03-28 13:10:47 +0545

    Updates

---
+++ precache-manifest.5a01f57e7d2411654eb561a36da55300.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c43712654204027b1ffb9c32014d4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc06fa0ea43127ccfd57",
+    "url": "/static/css/main.af6d9b4f.chunk.css"
+  },
+  {
+    "revision": "09dea8e776fa365f4c0c",
+    "url": "/static/js/2.e75bb167.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.e75bb167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc06fa0ea43127ccfd57",
+    "url": "/static/js/main.15dfa2c1.chunk.js"
+  },
+  {
+    "revision": "e034efadbb651f000ea6",
+    "url": "/static/js/runtime-main.deb660d4.js"
+  },
+  {
+    "revision": "42e1233fb2ee9492d91fd7495e6559f0",
+    "url": "/static/media/ankit.42e1233f.jpg"
+  },
+  {
+    "revision": "633ff0578bf7ce98b7a618bf6bea742f",
+    "url": "/static/media/aws-ccp.633ff057.png"
+  },
+  {
+    "revision": "68510a7acc412de9623d9c61a4d81f26",
+    "url": "/static/media/aws-csaa.68510a7a.png"
+  },
+  {
+    "revision": "335b251b7470d77f13951af1d30f16e6",
+    "url": "/static/media/ccent.335b251b.png"
+  },
+  {
+    "revision": "76a868d8e7de8ee392a80c93c41d9527",
+    "url": "/static/media/ccna.76a868d8.png"
+  },
+  {
+    "revision": "96e8f9252f9d6b49400c2c9cff9488cc",
+    "url": "/static/media/mcaaa.96e8f925.png"
+  },
+  {
+    "revision": "b93c8ca1c66f30be3ee3ca76ab45aa23",
+    "url": "/static/media/mcaf.b93c8ca1.png"
+  },
+  {
+    "revision": "bd928b03c7c460de862f4e12bd3b9cef",
+    "url": "/static/media/mcaif.bd928b03.png"
+  }
+]);
